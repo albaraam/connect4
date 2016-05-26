87ed62bc20ccadcee0db0cb27039d5bdeff608a2
Albaraa Mishlawi <albaraa_m@live.com>
2016-05-27 01:06:51 +0300

    init commit

---
+++ src/js/Player.js
@@ -0,0 +1,19 @@
+var Player = (function() {
+	var nextId = 1;
+	this.getRandomColor = function() {
+	    var letters = '0123456789ABCDEF'.split('');
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	    return color;
+	};
+	return function Player(name) {
+		// auto id
+		this.id = nextId;
+		this.name = (name !== null && name !== '') ? name : 'Player '+nextId;
+		this.color = getRandomColor();
+		// auto increment
+		nextId++;
+	};
+})();
